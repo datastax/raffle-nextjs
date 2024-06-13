aec066591c11bb4caccf24dd2505f32d84fa438b
crtr0 <carter.rabasa@gmail.com>
2024-06-13 16:14:15 -0700

    added favicon + gif, removed OpenAI

--- app/api/raffle/route.ts
+++ app/api/raffle/route.ts
@@ -1,32 +1,20 @@
 import { DataAPIClient } from "@datastax/astra-db-ts";
-import OpenAI from "openai";
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
     const json = await req.json()
     const { name, email } = json
 
-    const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
-    const embedding = await openai.embeddings.create({
-      model: "text-embedding-3-small",
-      input: name+email,
-      encoding_format: "float",
-    });
-
-    const vector = embedding.data[0].embedding
-
     const doc = {
       _id: email,
       name,
       email
     }
-
-    console.log(doc)
  
     const db = new DataAPIClient(process.env.ASTRA_DB_APPLICATION_TOKEN!).db(process.env.ASTRA_DB_API_ENDPOINT!)
     const collection = await db.collection("raffle")
-    const resp = await collection.insertOne(doc, { vector })
-    console.log(resp)
+    const resp = await collection.insertOne(doc, { vectorize: `${name}${email}` })
+    //console.log(resp)
 
     return NextResponse.json({ message: "OK" }, { status: 200 })
   }
--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Welcome to the DataStax && Vercel Raffle!
+          Welcome to the Raffle!
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
@@ -46,9 +46,10 @@ export default function Home() {
         </div>
       </div>
 
-      <div>
+      <div className="w-1/2">
       { submitted ?
-      <div>Thanks for entering our raffle, and GOOD LUCK! 🍀</div>
+      <div><img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHh4enp6d3BlanF2emp0d3Y1Z2Vzc2RsdnFwM3QxcG0wbXFyYTgyNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oeSAz6FqXCKuNFX6o/giphy.gif" alt="good luck"/>
+      </div>
       :
       <form action={enterRaffle} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div className="mb-4">
