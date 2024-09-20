907ac3c7f5c76e4c4d7e598d760220fad1038bd6
crtr0 <carter.rabasa@gmail.com>
2024-09-20 14:18:33 -0700

    generated a random vector for each entry

--- app/api/raffle/route.ts
+++ app/api/raffle/route.ts
@@ -4,8 +4,8 @@ import { NextResponse } from 'next/server'
 export async function POST(req: Request) {
     const json = await req.json()
     const { name, email, company } = json
-    const ip = req.headers.get('x-real-ip')
-    //console.log(ip)
+    const randomVector = Array.from(Array(1536), () => (Math.random() * 2) - 1)
+    console.log(randomVector)
     const db = new DataAPIClient(process.env.ASTRA_DB_APPLICATION_TOKEN!).db(process.env.ASTRA_DB_API_ENDPOINT!)
     const collection = await db.collection("raffle")
     const settings = await collection.findOne({ _id: "settings" })
@@ -14,7 +14,7 @@ export async function POST(req: Request) {
         name,
         email,
         company,
-        $vectorize: JSON.stringify({ name, company, ip }),
+        $vector: randomVector,
         created_at: new Date().toISOString()
       }
    
